5563aaec46188d8fbc97f5041eee4c56642eb17a
Diogo <dccarmo@gmail.com>
2017-07-23 12:14:12 -0300

    Added more typeable lines functions

--- src/__tests__/Boleto.test.ts
+++ src/__tests__/Boleto.test.ts
@@ -1,4 +1,8 @@
-import { BoletoType, getBoletoType, getBoletoTypeableLine } from "../models/Boleto";
+import { BoletoType,
+    getBoletoType,
+    getBoletoTypeableLine,
+    getBoletoTypeableLineSeqs,
+    getFormattedTypeableLine } from "../models/Boleto";
 
 const collectionBarCode = "83680000000560000820999989421070019693993499";
 const bankBarCode = "02191618900000166510010847800017732009402163";
@@ -22,5 +26,25 @@ describe("boleto model", () => {
     it("should return a bank boleto typeable line", () => {
         expect(getBoletoTypeableLine({ barCode: bankBarCode }))
         .toEqual("02190010884780001773420094021639161890000016651");
+    });
+
+    it("should return the collection boleto typeable line sequence", () => {
+        expect(getBoletoTypeableLineSeqs({ barCode: collectionBarCode }))
+        .toEqual(["83680000000", "9", "56000082099", "6", "99894210700", "9", "19693993499", "4"]);
+    });
+
+    it("should return the bank boleto typeable line sequence", () => {
+        expect(getBoletoTypeableLineSeqs({ barCode: bankBarCode }))
+        .toEqual(["02190", "01088", "47800", "017734", "20094", "021639", "1", "61890000016651"]);
+    });
+
+    it("should return the collection boleto formatted typeable line", () => {
+        expect(getFormattedTypeableLine({ barCode: collectionBarCode }))
+        .toEqual("83680000000-9 56000082099-6 99894210700-9 19693993499-4");
+    });
+
+    it("should return the bank boleto formatted typeable line", () => {
+        expect(getFormattedTypeableLine({ barCode: bankBarCode }))
+        .toEqual("02190.01088 47800.017734 20094.021639 1 61890000016651");
     });
 });
--- src/models/Boleto.ts
+++ src/models/Boleto.ts
@@ -19,41 +19,54 @@ export function getBoletoType(boleto: Boleto): BoletoType {
 }
 
 export function getBoletoTypeableLine(boleto: Boleto): string {
+    let seqs: string[];
+
+    if (getBoletoType(boleto) === BoletoType.Collection) {
+        seqs = getCollectionTypeableLineSeqs(boleto.barCode);
+    } else {
+        seqs = getBankTypeableLineSeq(boleto.barCode);
+    }
+
+    return seqs.reduce((typeableLine, seq) => typeableLine + seq);
+}
+
+export function getBoletoTypeableLineSeqs(boleto: Boleto): string[] {
+    if (getBoletoType(boleto) === BoletoType.Collection) {
+        return getCollectionTypeableLineSeqs(boleto.barCode);
+    }
+
+    return getBankTypeableLineSeq(boleto.barCode);
+}
+
+export function getFormattedTypeableLine(boleto: Boleto): string {
     if (getBoletoType(boleto) === BoletoType.Collection) {
         const seqs = getCollectionTypeableLineSeqs(boleto.barCode);
 
-        return seqs[0]
-        + seqs[1]
-        + seqs[2]
-        + seqs[3];
+        return `${seqs[0]}-${seqs[1]} ${seqs[2]}-${seqs[3]} ${seqs[4]}-${seqs[5]} ${seqs[6]}-${seqs[7]}`;
     }
-
     const seqs = getBankTypeableLineSeq(boleto.barCode);
 
-    return seqs[0]
-    + seqs[1]
-    + seqs[2]
-    + seqs[3]
-    + seqs[4];
+    return `${seqs[0]}.${seqs[1]} ${seqs[2]}.${seqs[3]} ${seqs[4]}.${seqs[5]} ${seqs[6]} ${seqs[7]}`;
 }
 
-function getCollectionTypeableLineSeqs(barCode: string): [string] {
-    let firstSeq = barCode.substring(0, 11);
-    firstSeq += checkdigit.mod10.create(firstSeq);
+function getCollectionTypeableLineSeqs(barCode: string): string[] {
+    const firstSeq = barCode.substring(0, 11);
+    const secondSeq = barCode.substring(11, 22);
+    const thirdSeq = barCode.substring(22, 33);
+    const fourthSeq = barCode.substring(33, 44);
 
-    let secondSeq = barCode.substring(11, 22);
-    secondSeq += checkdigit.mod10.create(secondSeq);
-
-    let thirdSeq = barCode.substring(22, 33);
-    thirdSeq += checkdigit.mod10.create(thirdSeq);
-
-    let fourthSeq = barCode.substring(33, 44);
-    fourthSeq += checkdigit.mod10.create(fourthSeq);
-
-    return [firstSeq, secondSeq, thirdSeq, fourthSeq];
+    return [firstSeq,
+        checkdigit.mod10.create(firstSeq),
+        secondSeq,
+        checkdigit.mod10.create(secondSeq),
+        thirdSeq,
+        checkdigit.mod10.create(thirdSeq),
+        fourthSeq,
+        checkdigit.mod10.create(fourthSeq),
+    ];
 }
 
-function getBankTypeableLineSeq(barCode: string): [string] {
+function getBankTypeableLineSeq(barCode: string): string[] {
     let firstSeq = barCode.substring(0, 4) + barCode.substring(19, 20) + barCode.substring(20, 24);
     firstSeq += checkdigit.mod10.create(firstSeq);
 
@@ -67,7 +80,21 @@ function getBankTypeableLineSeq(barCode: string): [string] {
 
     const fifthSeq = barCode.substring(5, 19);
 
-    return [firstSeq, secondSeq, thirdSeq, fourthSeq, fifthSeq];
+    const typeableLine = [firstSeq,
+        secondSeq,
+        thirdSeq,
+        fourthSeq,
+        fifthSeq].reduce((result, seq) => result + seq);
+
+    return [typeableLine.substring(0, 5),
+        typeableLine.substring(5, 10),
+        typeableLine.substring(10, 15),
+        typeableLine.substring(15, 21),
+        typeableLine.substring(21, 26),
+        typeableLine.substring(26, 32),
+        typeableLine.substring(32, 33),
+        typeableLine.substring(33, 47),
+    ];
 }
 
 export default Boleto;
